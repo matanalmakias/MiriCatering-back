722371d31eb66e869a69098184d2fe8edeb8b9a3
matanalmakias <inviteserv@gmail.com>
2025-11-10 19:46:28 +0200

    a

--- src/db/connect.js
+++ src/db/connect.js
@@ -8,12 +8,7 @@ const { HOST, DB, PORT, ROLES } = dbConfig;
 const connect = async () => {
   mongoose.set("strictQuery", false);
   await mongoose.connect(`mongodb://${HOST}:${PORT}/${DB}`);
-  // await mongoose.connect(
-  //   `mongodb+srv://inviteserv:WrI2zDwliVY986m2@cluster0.muivjeh.mongodb.net/TradingBot`
-  // );
-  // await mongoose.connect(
-  //   `mongodb://admin:RashenoreikSIsdisadi82387462621@127.0.0.1:27017/TradingBot?authSource=admin`
-  // );
+
   console.log(`Succesfully connected to the database ${DB}`);
 };
 
